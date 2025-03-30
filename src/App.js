1f54e1fe5d93171b018ecd4c6f2dcfb0edbf4413
hjyeeoonng <j10123@naevr.com>
2025-03-30 16:26:42 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>전현정의 블로그</h1>
-      </header>
+      <h1>전현정의 홈페이지</h1>
     </div>
   );
 }
